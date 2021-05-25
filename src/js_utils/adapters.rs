2cfa68b80c9a4a33ceb802e0af34cce237f72bb2
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-05-25 13:44:53 +0200

    wip

--- src/js_utils/adapters.rs
+++ src/js_utils/adapters.rs
@@ -1,45 +1,76 @@
 use crate::js_utils::{JsError, Script};
 
-pub trait JsRuntimeAdapter {}
+pub trait JsRuntimeAdapter {
+    type JsValueAdapterType: JsValueAdapter;
+    type JsContextAdapterType: JsContextAdapter;
+
+    fn js_create_context(&self, id: &str) -> Result<Box<Self::JsValueAdapterType>, JsError>;
+    fn js_get_context(&self, id: &str) -> Option<Box<Self::JsValueAdapterType>>;
+    fn js_get_main_context(&self) -> Option<&Self::JsContextAdapterType>;
+}
+
+pub type JsFunction =
+    dyn Fn(dyn JsValueAdapter, Vec<dyn JsValueAdapter>) -> Result<dyn JsValueAdapter, JsError>;
 
 pub trait JsContextAdapter {
-    fn eval(&self, script: Script, return_value: &mut dyn JsValueAdapter) -> Result<(), JsError>;
-    fn eval_module(
+    type JsRuntimeAdapterType: JsRuntimeAdapter;
+
+    fn js_eval(
+        &self,
+        script: Script,
+    ) -> Result<
+        <<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType,
+        JsError,
+    >;
+
+    fn js_install_function<
+        F: Fn(
+            <<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType,
+            Vec<<<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType>,
+        ) -> Result<<<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType, JsError>,
+    >(
+        &self,
+        namespace: Vec<&str>,
+        name: &str,
+        js_function: F,
+        arg_count: u32,
+    ) -> Result<(), JsError>;
+    fn js_eval_module(
         &self,
         script: Script,
         return_value: &mut dyn JsValueAdapter,
     ) -> Result<(), JsError>;
-    fn get_namespace(
+    fn js_get_namespace(
         &self,
         namespace: &[&str],
         return_value: &mut dyn JsValueAdapter,
     ) -> Result<(), JsError>;
-    fn invoke_function(
+    fn js_invoke_function(
         &self,
         namespace: &[&str],
         method_name: &str,
         args: &[&dyn JsValueAdapter],
         return_value: &mut dyn JsValueAdapter,
     ) -> Result<(), JsError>;
-    fn invoke_function2(
+    fn js_invoke_function2(
         &self,
         this_obj: &dyn JsValueAdapter,
         method_name: &str,
         args: &[&dyn JsValueAdapter],
         return_value: &mut dyn JsValueAdapter,
     ) -> Result<(), JsError>;
-    fn get_object_property(
+    fn js_get_object_property(
         &self,
         object: &dyn JsValueAdapter,
         property_name: &str,
         return_value: &mut dyn JsValueAdapter,
     ) -> Result<(), JsError>;
-    fn delete_object_property(
+    fn js_delete_object_property(
         &self,
         object: &dyn JsValueAdapter,
         property_name: &str,
     ) -> Result<(), JsError>;
-    fn set_object_property(
+    fn js_set_object_property(
         &self,
         object: &dyn JsValueAdapter,
         property_name: &str,
@@ -48,16 +79,16 @@ pub trait JsContextAdapter {
 }
 
 pub trait JsValueAdapter {
-    fn is_bool(&self) -> bool;
-    fn is_i32(&self) -> bool;
-    fn is_f64(&self) -> bool;
-    fn is_object(&self) -> bool;
-    fn is_string(&self) -> bool;
-    fn is_function(&self) -> bool;
-    fn is_bigint(&self) -> bool;
-    fn type_of(&self) -> &'static str;
-    fn to_bool(&self) -> bool;
-    fn to_i32(&self) -> i32;
-    fn to_f64(&self) -> f64;
-    fn to_string(&self) -> String;
+    fn js_is_bool(&self) -> bool;
+    fn js_is_i32(&self) -> bool;
+    fn js_is_f64(&self) -> bool;
+    fn js_is_object(&self) -> bool;
+    fn js_is_string(&self) -> bool;
+    fn js_is_function(&self) -> bool;
+    fn js_is_bigint(&self) -> bool;
+    fn js_type_of(&self) -> &'static str;
+    fn js_to_bool(&self) -> bool;
+    fn js_to_i32(&self) -> i32;
+    fn js_to_f64(&self) -> f64;
+    fn js_to_string(&self) -> String;
 }
--- src/js_utils/facades.rs
+++ src/js_utils/facades.rs
@@ -5,16 +5,29 @@ use std::future::Future;
 pub struct JsProxy {}
 
 pub trait JsRuntimeFacade {
+    type JsRuntimeAdapterType: JsRuntimeAdapter;
     type ContextFacadeType: JsContextFacade;
+
     fn js_create_context(&self) -> Self::ContextFacadeType;
-    fn js_loop_exe<R, C: FnOnce(&dyn JsRuntimeAdapter) -> R>(&self, consumer: C) -> R;
+    fn js_loop_exe<
+        R: Send + 'static,
+        C: FnOnce(&Self::JsRuntimeAdapterType) -> R + Send + 'static,
+    >(
+        &self,
+        consumer: C,
+    ) -> R;
     fn js_loop_add(&self);
 }
 
 pub trait JsContextFacade {
+    type JsRuntimeAdapterType: JsRuntimeAdapter;
+
     fn js_install_proxy(&self, js_proxy: JsProxy);
     fn js_eval(&self, script: Script) -> Box<dyn Future<Output = Result<JsValueFacade, JsError>>>;
-    fn js_loop_exe<R, C: FnOnce(&dyn JsRuntimeAdapter, &dyn JsContextAdapter) -> R>(
+    fn js_loop_exe<
+        R,
+        C: FnOnce(&Self::JsRuntimeAdapterType, &<<Self as JsContextFacade>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsContextAdapterType) -> R,
+    >(
         &self,
         consumer: C,
     ) -> R;
