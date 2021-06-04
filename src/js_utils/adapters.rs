d0175555cc690a8e268d2312058c8dd92509b3cb
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-06-04 12:40:30 +0200

    wip

--- src/js_utils/adapters.rs
+++ src/js_utils/adapters.rs
@@ -25,6 +25,7 @@ pub trait JsContextAdapter {
 
     fn js_install_function<
         F: Fn(
+            <<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsContextAdapterType,
             <<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType,
             Vec<<<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType>,
         ) -> Result<<<Self as JsContextAdapter>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsValueAdapterType, JsError>,
