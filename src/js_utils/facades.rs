61c5413e6aa60b5581b9c78fa25d3936fc9809f9
Andries Hiemstra <andries@fam-hiemstra.nl>
2021-06-15 17:44:28 +0200

    wip

--- src/js_utils/facades.rs
+++ src/js_utils/facades.rs
@@ -1,37 +1,89 @@
+use crate::eventloop::EventLoop;
 use crate::js_utils::adapters::{JsContextAdapter, JsRuntimeAdapter, JsValueAdapter};
 use crate::js_utils::{JsError, Script};
 use std::future::Future;
+use std::marker::PhantomData;
 
 pub struct JsProxy {}
 
-pub trait JsRuntimeFacade {
+pub trait JsRuntimeBuilder {
     type JsRuntimeAdapterType: JsRuntimeAdapter;
-    type ContextFacadeType: JsContextFacade;
-
-    fn js_create_context(&self) -> Self::ContextFacadeType;
-    fn js_loop_exe<
-        R: Send + 'static,
-        C: FnOnce(&Self::JsRuntimeAdapterType) -> R + Send + 'static,
-    >(
-        &self,
-        consumer: C,
-    ) -> R;
-    fn js_loop_add(&self);
+    fn build(&self);
+    fn with_rt<R, F: FnOnce(&Self::JsRuntimeAdapterType) -> R>(&self, consumer: F) -> R;
 }
 
-pub trait JsContextFacade {
-    type JsRuntimeAdapterType: JsRuntimeAdapter;
+pub struct JsRuntimeFacade<S: JsRuntimeAdapter> {
+    event_loop: EventLoop,
+    _phantom: PhantomData<S>,
+}
 
-    fn js_install_proxy(&self, js_proxy: JsProxy);
-    fn js_eval(&self, script: Script) -> Box<dyn Future<Output = Result<JsValueFacade, JsError>>>;
-    fn js_loop_exe<
-        R,
-        C: FnOnce(&Self::JsRuntimeAdapterType, &<<Self as JsContextFacade>::JsRuntimeAdapterType as JsRuntimeAdapter>::JsContextAdapterType) -> R,
-    >(
+impl<S: JsRuntimeAdapter> JsRuntimeFacade<S> {
+    pub fn new(builder: impl JsRuntimeBuilder<JsRuntimeAdapterType = S> + Send + 'static) -> Self {
+        let ret = Self {
+            event_loop: EventLoop::new(),
+            _phantom: Default::default(),
+        };
+
+        ret.event_loop.exe(move || {
+            // init thread local with builder.build
+            // todo move builder to tl var, builder should have a with_rt() fn
+            builder.build();
+        });
+
+        ret
+    }
+    pub fn js_create_context(&self, _name: &str) -> JsContextFacade {
+        todo!()
+    }
+    pub fn js_get_main_context(&self) -> &JsContextFacade {
+        todo!()
+    }
+    pub fn js_get_context(&self, _name: &str) -> JsContextFacade {
+        todo!()
+    }
+    pub fn js_loop_exe<R: Send + 'static, C: FnOnce(&S) -> R + Send + 'static>(
+        &self,
+        _consumer: C,
+    ) -> R {
+        todo!()
+    }
+    pub fn js_loop(&self) {
+        todo!()
+    }
+    pub fn js_loop_sync(&self) {
+        todo!()
+    }
+}
+
+pub struct JsContextFacade {}
+
+impl JsContextFacade {
+    pub fn js_install_proxy(&self, _js_proxy: JsProxy) {
+        todo!()
+    }
+    pub fn js_eval(
+        &self,
+        _script: Script,
+    ) -> Box<dyn Future<Output = Result<JsValueFacade, JsError>>> {
+        todo!()
+    }
+    /*
+    pub fn js_loop_sync<R, C: FnOnce(&dyn JsRuntimeAdapter, &dyn JsContextAdapter) -> R>(
         &self,
         consumer: C,
-    ) -> R;
-    fn js_loop_add(&self);
+    ) -> R {
+        todo!()
+    }
+    pub fn js_loop<R, C>(&self) -> Box<dyn Future<Output = Result<R, JsError>>>
+    where
+        C: FnOnce(&dyn JsRuntimeAdapter, &dyn JsContextAdapter) -> Result<R, JsError>,
+    {
+        todo!()
+    }
+    */
+    pub fn js_loop_void(&self) {
+        todo!()
+    }
 }
 
 pub struct JsValueFacade {}
